293b08e01a2aa23ce5724bdcfa909ac8cba7d429
Nguyen <asn579@15.0-26.89.116.129.in-addr.arpa>
2016-10-25 13:03:28 -0500

    added greeting funciton

--- bot.js
+++ bot.js
@@ -18,6 +18,42 @@ function respond() {
   }
 }
 
+function greeting ()
+{
+  var botResponse, options, body, botReq;
+
+  botResponse = "hello";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text"   : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 function postMessage() {
   var botResponse, options, body, botReq;
 
