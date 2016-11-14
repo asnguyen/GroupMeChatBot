a2ae43d7445f0692b57dfb7982621410b4630b78
Nguyen <asn579@acts-853619.local>
2016-11-13 21:00:04 -0600

    added url_test() method

--- bot.js
+++ bot.js
@@ -8,16 +8,7 @@ function respond() {
       botRegex1 = /^\/test$/,
       botRegex2 = /^\/hello alfred$/
       botRegex3 = /^\/goodbye alfred$/;
-
-  /*if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
-  }*/
+      botRegex4 = /^\/url$/;
 
   if(request.text)
   {
@@ -34,6 +25,10 @@ function respond() {
     {
       farewell();
     }
+    if(botRegex4.test(request.text))
+    {
+      url_test();
+    }
     this.res.end();
   }
   else
@@ -43,6 +38,38 @@ function respond() {
     this.res.end();
   }
 
+}
+
+function url_test()
+{
+  var botResponse, options, body, botReq;
+  botResponse = "https://www.facebook.com/photo.php?fbid=1045289238897959&set=a.107157549377804.13789.100002506791205&type=3&theater";
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+  body = {
+    "bot_id" : botID;
+    "text"   : botResponse;
+  };
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function greeting()
